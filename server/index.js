4e0a42b80705988ed82f9b4d63b3dc9986979e19
Andrii <ne.mogu.pridymay@gmail.com>
2023-05-24 01:17:04 +0200

    add server

---
+++ server/index.js
@@ -0,0 +1,39 @@
+require('dotenv').config();
+
+const express = require('express');
+const sequelize = require('./db');
+const cors = require('cors');
+const http = require('http');
+const {Server} = require('socket.io');
+const path = require('path');
+
+const fileUpload = require('express-fileupload');
+
+const app = express();
+
+app.use(cors({origin: '*'}))
+app.use(express.json());
+app.use(express.static(path.resolve(__dirname, 'static')));
+app.use(fileUpload({}));
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: {
+        origin: '*',
+    }
+})
+
+const start = async () => {
+    try{
+        await sequelize.authenticate();
+        await sequelize.sync();
+        server.listen(process.env.PORT, () => {
+            console.log(`server is running on port ${process.env.PORT}`);
+        })
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+start();
